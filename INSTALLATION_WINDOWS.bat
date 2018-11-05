df9d371ef293cbb602aa84438222c2b4aa326dd8
thomasdenecker <thomas.denecker@gmail.com>
2018-11-05 16:49:26 +0100

    Remove automatic script to stop all docker

--- INSTALLATION_WINDOWS.bat
+++ INSTALLATION_WINDOWS.bat
@@ -3,6 +3,5 @@ docker run --name PIXEL2_DB -d tdenecker/pixel2_db
 docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" PIXEL2_DB > Database/ipDB.txt
 
 docker pull tdenecker/pixel2_app
-echo FOR /F "tokens=*" %%%%g IN ('docker ps -a -q') do (docker stop %%%%g) > Pixel2.bat
 echo docker start PIXEL2_DB >> Pixel2.bat
 echo docker run --rm --link  PIXEL2_DB:postgres -p 3838:3838 -v %CD%:/srv/shiny-server tdenecker/pixel2_app >> Pixel2.bat
