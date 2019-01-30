64de2d5ae79194e4b9c3aaeccee469e4eeca610d
thomasdenecker <thomas.denecker@gmail.com>
2019-01-30 17:04:12 +0100

    Update installation script (detach)

--- INSTALLATION_WINDOWS.bat
+++ INSTALLATION_WINDOWS.bat
@@ -1,8 +1,9 @@
 docker pull tdenecker/pixel2_db
 docker run --name PIXEL2_DB -d tdenecker/pixel2_db
-docker start PIXEL2_DB 
+docker start PIXEL2_DB
 docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" PIXEL2_DB > Database/ipDB.txt
 
 docker pull tdenecker/pixel2_app
+echo docker stop PIXEL2_DB >> Pixel2.bat
 echo docker start PIXEL2_DB >> Pixel2.bat
-echo docker run --rm --link  PIXEL2_DB:postgres -p 3838:3838 -v %CD%:/srv/shiny-server tdenecker/pixel2_app >> Pixel2.bat
+echo docker run -d --rm --link  PIXEL2_DB:postgres -p 3838:3838 -v %CD%:/srv/shiny-server tdenecker/pixel2_app >> Pixel2.bat
